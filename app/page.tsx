fe6bbb431f924c068d7d35e5208ca4781aeabc34
Christophe Lablancherie <clablancherie@crakmedia.com>
2023-05-04 22:13:12 -0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/app/components/navbar/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <div className="text-rose-500 text-2xl">Hello Air Bnb</div>
+    </div>
+  )
+}
